0d0e1600b0ade605b35f192462e8d840d78b33c5
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-09-27 12:38:14 +0300

    Initial commit

---
+++ src/main/java/TenMinWalk.java
@@ -0,0 +1,13 @@
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+        // Insert brilliant code here
+        if (walk.length != 10) return false;
+
+
+
+
+            return true;
+
+
+    }
+}
---
+++ src/test/java/TenMinWalkTest_1.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TenMinWalkTest_1 {
+
+
+
+        @Test
+        public void Test() {
+            assertEquals(true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}));
+            assertEquals(false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
+            assertEquals( false, TenMinWalk.isValid(new char[] {'w'}));
+            assertEquals( false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+        }
+    }
+
