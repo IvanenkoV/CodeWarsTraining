a31b084fac83898b1b858f94941d80e08e9daeec
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-09-27 16:54:04 +0300

    Adding Code wars  Ten Min Walk training

--- src/main/java/TenMinWalk.java
+++ src/main/java/TenMinWalk.java
@@ -3,11 +3,28 @@ public class TenMinWalk {
         // Insert brilliant code here
         if (walk.length != 10) return false;
 
+        int nCount = 0, sCount = 0, wCount = 0, eCount = 0;
 
+        for (char c : walk) {
+            switch (c) {
+                case 'n':
+                    nCount++;
+                    break;
+                case 's':
+                    sCount++;
+                    break;
+                case 'w':
+                    wCount++;
+                    break;
+                case 'e':
+                    eCount++;
+                    break;
+            }
 
+        }
 
-            return true;
+        if (nCount == sCount && wCount == eCount) return true;
 
-
+        else return false;
     }
 }
