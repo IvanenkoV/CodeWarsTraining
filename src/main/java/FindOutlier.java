ac5c9002b82168f041e123b6e27885f404999c5b
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-10-18 09:36:18 +0300

    Adding Code wars  FindOutlier training FINISHED

--- src/main/java/FindOutlier.java
+++ src/main/java/FindOutlier.java
@@ -1,11 +1,17 @@
-public class FindOutlier {
+public class FindOutlier{
     static int find(int[] integers){
         int n = 0;
-        if (integers[0] % 2 == 0 || integers[1] % 2 == 0 )
+        if (integers[0] % 2 == 0 && integers[1] % 2 == 0 || integers[1] % 2 == 0 && integers[2] % 2 == 0  )
         {
             for( int i: integers){
                 if (i % 2 != 0) n = i;
-                System.out.println(i % 2);
+            }
+
+        }
+        if (integers[0] % 2 != 0 && integers[1] % 2 != 0 || integers[1] % 2 != 0 && integers[2] % 2 != 0  )
+        {
+            for( int i: integers){
+                if (i % 2 == 0) n = i;
             }
 
         }
