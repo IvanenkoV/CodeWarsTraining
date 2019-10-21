50281f4d5633a200752bcda1ba4653fb75ded2ab
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-10-21 09:42:29 +0300

    Adding Code wars  DuplicateEncoder my version final With coments and more clear

--- src/main/java/DuplicateEncoder.java
+++ src/main/java/DuplicateEncoder.java
@@ -7,30 +7,27 @@ public class DuplicateEncoder {
         String strLower = word.toLowerCase();
         char[] arr = strLower.toCharArray();
         String buffer = new String(strLower);
+
+        // In map will be saved symbols and boolean statement true if there are more then 1 such symbol in the string
         Map<String, Boolean> howManyMap = new HashMap<>();
 
+        // Filling the map
         for (int i = 0; i < buffer.length(); ) {
 
             if (buffer.indexOf(buffer.charAt(i), i + 1) != -1) {
                 howManyMap.put(String.valueOf(buffer.charAt(i)), true);
-
             } else {
-
                 howManyMap.put(String.valueOf(buffer.charAt(i)), false);
             }
             buffer = buffer.replace(String.valueOf(buffer.charAt(i)), "");
-
         }
-        System.out.println(howManyMap);
-
+        // Forming of an array with answer
         for (int i = 0; i < arr.length; i++) {
             if (howManyMap.get(String.valueOf(arr[i]))) {
                 arr[i] = ')';
             } else arr[i] = '(';
         }
         String result = new String(arr);
-
-
         return result;
     }
 }
