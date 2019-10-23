98842f4c4f3af07ad29376f45944222551d87ffa
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-10-23 14:54:40 +0300

    Adding Code wars  SumSquaredDivisors Mine variant FINAL

--- src/main/java/SumSquaredDivisors.java
+++ src/main/java/SumSquaredDivisors.java
@@ -7,38 +7,28 @@ public class SumSquaredDivisors {
 
     public static String listSquared(long m, long n) {
 
-        String resultString;
         List<String> results = new ArrayList<>();
 
         for (long i = m; i < n; i++) {
             List<Long> arraySubDivisors = new ArrayList<>();
 
+            // List of values of natural divisors
             for (long j = 1; j <= i; j++) {
                 if (i % j == 0) arraySubDivisors.add(j);
             }
-            List<Long> list2 = new ArrayList<>();
+            // the sum of divisors
             long sum = 0;
             for (int k = 0; k < arraySubDivisors.size(); k++) {
-                list2.add(arraySubDivisors.get(k) * arraySubDivisors.get(k));
-                sum += list2.get(k);
+                sum += arraySubDivisors.get(k) * arraySubDivisors.get(k);
             }
-
             double sumSqrt = sqrt(sum);
+
+            // Filling a list with result values for numbers whose squared divisors is a square and then the sum of the squared divisors.
             if (sumSqrt == (long) sumSqrt) {
-
                 results.add("[" + i + ", " + sum + "]");
-
             }
         }
 
-        resultString = "[";
-        for (int i = 0; i < results.size(); i++) {
-            resultString = resultString + results.get(i);
-            if (i == results.size() - 1) continue;
-            resultString += ", ";
-        }
-
-        resultString += "]";
-        return resultString;
+        return results.toString();
     }
 }
