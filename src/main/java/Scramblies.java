3c745d7cf453ff655cf4670c07addfd1720ba752
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-10-24 15:04:02 +0300

    Adding Code wars  Scramblies  Mine variant FINAL

--- src/main/java/Scramblies.java
+++ src/main/java/Scramblies.java
@@ -1,8 +1,22 @@
 public class Scramblies {
 
     public static boolean scramble(String str1, String str2) {
+        char[] string1 = str1.toCharArray();
+        char[] string2 = str2.toCharArray();
+        int counter = 0;
+        for (int i = 0; i < string2.length; i++) {
 
-        // your code
-        return true;
+            for (int j = 0; j < string1.length; j++) {
+
+                if (string2[i] == string1[j]) {
+                    counter++;
+                    string1[j] = '0';
+                    break;
+                }
+            }
+        }
+
+        if (string2.length - counter == 0) return true;
+        else return false;
     }
 }
