a844a091e773c21f6bc3677c232c81f4c663bb2c
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-10-25 11:04:29 +0300

    Adding Code wars  Scramblies  Not Mine with using splic and replaceFirst

---
+++ src/main/java/HashConversion.java
@@ -0,0 +1,20 @@
+//  for using hash function
+
+//<dependency>
+//<groupId>com.google.guava</groupId>
+//<artifactId>guava</artifactId>
+//<version>21.0</version>
+//</dependency>
+//import com.google.common.base.Charsets;
+//import com.google.common.hash.HashFunction;
+//import com.google.common.hash.Hashing;
+
+
+
+//import com.google.common.base.Charsets;
+//import com.google.common.hash.HashFunction;
+//import com.google.common.hash.Hashing;
+//public class HashConversion {
+//
+//    HashFunction hf = Hashing.murmur3_128(key); // can choose any hash function
+//}
