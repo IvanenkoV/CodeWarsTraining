e4d2615d828fd6defc549c188a9f279a88737980
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-11-05 14:25:51 +0200

    Adding Code wars  Dictionary My variant (LEVENSTAIN ALGORITHM to find maximal similar world from the string

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -10,34 +10,52 @@ public class Dictionary {
 
     public String findMostSimilar(String to) {
 
-        System.out.println(Arrays.toString(words));
-        System.out.println(to);
+        int[] minimalActions = new int[words.length];
+        int[] minimalActionsSorted = new int[words.length];
+        String result = "";
 
-        int [] arraySimilarity = new int[words.length];
-        int  similarity;
-        for (int i = 0; i < words.length; i ++){
-             similarity = 0;
-            String separateWord = words[i];
-            for (int j = 0;  j < to.length(); j ++){
-
-                for (int k = 0; k < separateWord.length(); ){
-
-
-                    if (to.charAt(j) == separateWord.charAt(k))
-                    {
-                        similarity++;
-                        separateWord = separateWord.replaceFirst(String.valueOf( separateWord.charAt(k)), "");
-                        break;
-                    }
-                    k ++;
-                }
-
-            }
-            arraySimilarity[i] = similarity;
+        //  Array with minimal moves for each word to compare it with word for search
+        for (int i = 0; i < words.length; i++) {
+            minimalActions[i] = levenstain(words[i], to);
         }
-        System.out.println(Arrays.toString(arraySimilarity));
+        System.arraycopy(minimalActions, 0, minimalActionsSorted, 0, minimalActions.length);
+        Arrays.sort(minimalActionsSorted);
 
+        for (int i = 0; i < minimalActions.length; i++) {
+            if (minimalActions[i] == minimalActionsSorted[0]) result = words[i];
+        }
 
-        return null;
+        return result;
+
+        // Levenstain algorithm to find minimal changes between two strings to match them
+    }
+
+    public static int levenstain(String str1, String str2) {
+        int[] Di_1 = new int[str2.length() + 1];
+        int[] Di = new int[str2.length() + 1];
+
+        for (int j = 0; j <= str2.length(); j++) {
+            Di[j] = j; // (i == 0)
+        }
+
+        for (int i = 1; i <= str1.length(); i++) {
+            System.arraycopy(Di, 0, Di_1, 0, Di_1.length);
+
+            Di[0] = i; // (j == 0)
+            for (int j = 1; j <= str2.length(); j++) {
+                int cost = (str1.charAt(i - 1) != str2.charAt(j - 1)) ? 1 : 0;
+                Di[j] = min(
+                        Di_1[j] + 1,
+                        Di[j - 1] + 1,
+                        Di_1[j - 1] + cost
+                );
+            }
+        }
+
+        return Di[Di.length - 1];
+    }
+
+    private static int min(int n1, int n2, int n3) {
+        return Math.min(Math.min(n1, n2), n3);
     }
 }
